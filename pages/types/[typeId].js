b580c340594a6d1140dc82ddb85d772b0bcc867a
pablohen <pablo_hen@hotmail.com>
2021-03-16 21:03:44 -0300

    add links, classes

--- pages/types/[typeId].js
+++ pages/types/[typeId].js
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router';
 import pokeapiService from '../../services/pokeapiService';
 import MainMenu from '../../components/MainMenu';
 import TitleBar from '../../components/TitleBar';
+import Link from 'next/link';
 
 const TypeDetailsPage = () => {
   const router = useRouter();
@@ -26,65 +27,93 @@ const TypeDetailsPage = () => {
       <MainMenu />
       <TitleBar title={`${type?.name} type`} />
 
-      <div>
-        <span>Double damage from:</span>
-        <ul>
+      <div className="bg-gray-200 p-4 rounded-xl">
+        <span className="font-semibold">Double damage from:</span>
+        <div className="space-x-2 pt-2">
           {type?.damage_relations?.double_damage_from?.map((otherType) => (
-            <li key={otherType.name}>{otherType.name}</li>
+            <Link key={otherType.name} href={`/types/${otherType.name}`}>
+              <a className="px-6 py-1 bg-gray-800 rounded-xl text-white capitalize">
+                {otherType.name}
+              </a>
+            </Link>
           ))}
-        </ul>
+        </div>
       </div>
 
       <div>
         <span>Double damage to:</span>
-        <ul>
+        <div className="space-x-2 pt-2">
           {type?.damage_relations?.double_damage_to?.map((otherType) => (
-            <li key={otherType.name}>{otherType.name}</li>
+            <Link key={otherType.name} href={`/types/${otherType.name}`}>
+              <a className="px-6 py-1 bg-gray-800 rounded-xl text-white capitalize">
+                {otherType.name}
+              </a>
+            </Link>
           ))}
-        </ul>
+        </div>
       </div>
 
       <div>
         <span>Half damage from:</span>
-        <ul>
+        <div className="space-x-2 pt-2">
           {type?.damage_relations?.half_damage_from?.map((otherType) => (
-            <li key={otherType.name}>{otherType.name}</li>
+            <Link key={otherType.name} href={`/types/${otherType.name}`}>
+              <a className="px-6 py-1 bg-gray-800 rounded-xl text-white capitalize">
+                {otherType.name}
+              </a>
+            </Link>
           ))}
-        </ul>
+        </div>
       </div>
 
       <div>
         <span>Half damage to:</span>
-        <ul>
+        <div className="space-x-2 pt-2">
           {type?.damage_relations?.half_damage_to?.map((otherType) => (
-            <li key={otherType.name}>{otherType.name}</li>
+            <Link key={otherType.name} href={`/types/${otherType.name}`}>
+              <a className="px-6 py-1 bg-gray-800 rounded-xl text-white capitalize">
+                {otherType.name}
+              </a>
+            </Link>
           ))}
-        </ul>
+        </div>
       </div>
 
       <div>
         <span>No damage from:</span>
-        <ul>
+        <div className="space-x-2 pt-2">
           {type?.damage_relations?.no_damage_from?.map((otherType) => (
-            <li key={otherType.name}>{otherType.name}</li>
+            <Link key={otherType.name} href={`/types/${otherType.name}`}>
+              <a className="px-6 py-1 bg-gray-800 rounded-xl text-white capitalize">
+                {otherType.name}
+              </a>
+            </Link>
           ))}
-        </ul>
+        </div>
       </div>
 
       <div>
         <span>No damage to:</span>
-        <ul>
+        <div className="space-x-2 pt-2">
           {type?.damage_relations?.no_damage_to?.map((otherType) => (
-            <li key={otherType.name}>{otherType.name}</li>
+            <Link key={otherType.name} href={`/types/${otherType.name}`}>
+              <a className="px-6 py-1 bg-gray-800 rounded-xl text-white capitalize">
+                {otherType.name}
+              </a>
+            </Link>
           ))}
-        </ul>
+        </div>
       </div>
 
       <div>
         <span>Moves:</span>
         <ul>
           {type?.moves?.map((move) => (
-            <li key={move.name}>{move.name}</li>
+            <li key={move.name}>
+              <Link href={`/moves/${move.name}`}>
+                <a className="capitalize">{move.name}</a>
+              </Link>
+            </li>
           ))}
         </ul>
       </div>
@@ -92,8 +121,12 @@ const TypeDetailsPage = () => {
       <div>
         <span>Pokemon:</span>
         <ul>
-          {type?.pokemon?.map((item) => (
-            <li key={item.pokemon.name}>{item.pokemon.name}</li>
+          {type?.pokemon?.map(({ pokemon }) => (
+            <li key={pokemon.name}>
+              <Link href={`/pokemon/${pokemon.name}`}>
+                <a className="capitalize">{pokemon.name}</a>
+              </Link>
+            </li>
           ))}
         </ul>
       </div>
