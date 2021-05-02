936a20d1bedafa0a0c2bd8a2d395f42c1bcb97c0
pablohen <pablo_hen@hotmail.com>
2021-05-02 15:01:17 -0300

    generating all pokemon pages statically on build

--- pages/pokemon/[pokemonId].jsx
+++ pages/pokemon/[pokemonId].jsx
@@ -1,31 +1,60 @@
-import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
+import React from 'react';
+import PropTypes from 'prop-types';
 import pokeapiService from '../../services/pokeapiService';
 import PokemonDetails from '../../components/PokemonDetails';
 import MainMenu from '../../components/MainMenu';
 
-const PokemonPage = () => {
-  const router = useRouter();
-  const { pokemonId } = router.query;
-  const [pokemon, setPokemon] = useState({});
+const PokemonPage = ({ pokemon }) => (
+  // const router = useRouter();
+  // const { pokemonId } = router.query;
+  // const [pokemon, setPokemon] = useState({});
 
-  const getPokemonData = async (selectedPokemon) => {
-    const pokemonData = await pokeapiService.getPokemon(selectedPokemon);
-    setPokemon(pokemonData);
+  // const getPokemonData = async (selectedPokemon) => {
+  //   const pokemonData = await pokeapiService.getPokemon(selectedPokemon);
+  //   setPokemon(pokemonData);
+  // };
+
+  // useEffect(() => {
+  //   if (pokemonId) {
+  //     getPokemonData(pokemonId);
+  //   }
+  // }, [pokemonId]);
+
+  <div>
+    <MainMenu />
+    <PokemonDetails pokemon={pokemon} />
+  </div>
+);
+
+PokemonPage.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
+  pokemon: PropTypes.any.isRequired,
+};
+
+export const getStaticPaths = async () => {
+  const pokedex = await pokeapiService.getPokedex('national');
+  const paths = pokedex.pokemon_entries.map((pokemon) => ({
+    params: {
+      pokemonId: pokemon.pokemon_species.name,
+    },
+  }));
+
+  return {
+    paths,
+    fallback: true,
   };
+};
 
-  useEffect(() => {
-    if (pokemonId) {
-      getPokemonData(pokemonId);
-    }
-  }, [pokemonId]);
+export const getStaticProps = async ({ params }) => {
+  const { pokemonId } = params;
 
-  return (
-    <div>
-      <MainMenu />
-      <PokemonDetails pokemon={pokemon} />
-    </div>
-  );
+  const pokemonData = await pokeapiService.getPokemon(pokemonId);
+
+  return {
+    props: {
+      pokemon: pokemonData,
+    },
+  };
 };
 
 export default PokemonPage;
