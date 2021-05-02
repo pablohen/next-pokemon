f4f615eed3ccb09a792f9ef0c237673ade343def
pablohen <pablo_hen@hotmail.com>
2021-05-02 14:33:29 -0300

    testing getStaticPaths performance

--- pages/pokedex/[pokedexId].jsx
+++ pages/pokedex/[pokedexId].jsx
@@ -1,45 +1,73 @@
-import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
+import React from 'react';
 import Link from 'next/link';
+import PropTypes from 'prop-types';
 import pokeapiService from '../../services/pokeapiService';
 import MainMenu from '../../components/MainMenu';
 import TitleBar from '../../components/TitleBar';
 
-const PokedexPage = () => {
-  const router = useRouter();
-  const { pokedexId } = router.query;
-  const [pokedex, setPokedex] = useState({});
+const PokedexPage = ({ pokedex }) => (
+  // const router = useRouter();
+  // const { pokedexId } = router.query;
+  // const [pokedex, setPokedex] = useState({});
 
-  const getPokedexData = async (selectedPokedex) => {
-    const pokedexData = await pokeapiService.getPokedex(selectedPokedex);
-    setPokedex(pokedexData);
-  };
+  // const getPokedexData = async (selectedPokedex) => {
+  //   const pokedexData = await pokeapiService.getPokedex(selectedPokedex);
+  //   setPokedex(pokedexData);
+  // };
 
-  useEffect(async () => {
-    if (pokedexId) {
-      await getPokedexData(pokedexId);
-    }
-  }, [pokedexId]);
+  // useEffect(async () => {
+  //   if (pokedexId) {
+  //     await getPokedexData(pokedexId);
+  //   }
+  // }, [pokedexId]);
 
-  return (
-    <div>
-      <MainMenu />
-      <TitleBar title={`${pokedex?.names?.[0].name} Dex`} />
+  <div>
+    <MainMenu />
+    <TitleBar title={`${pokedex?.names?.[0].name} Dex`} />
 
-      <div className="px-4">
-        <ul>
-          {pokedex?.pokemon_entries?.map((pokemon) => (
-            <li key={pokemon.entry_number}>
-              <Link href={`../pokemon/${pokemon.pokemon_species.name}`}>
-                <button type="button" className="capitalize p-2">
-                  {pokemon.pokemon_species.name}
-                </button>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
+    <div className="px-4">
+      <ul>
+        {pokedex?.pokemon_entries?.map((pokemon) => (
+          <li key={pokemon.entry_number}>
+            <Link href={`../pokemon/${pokemon.pokemon_species.name}`}>
+              <button type="button" className="capitalize p-2">
+                {pokemon.pokemon_species.name}
+              </button>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
-  );
+  </div>
+);
+
+PokedexPage.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
+  pokedex: PropTypes.any.isRequired,
 };
+
+export const getStaticPaths = async () => {
+  const pokedexesData = await pokeapiService.getPokedexes();
+
+  const paths = pokedexesData.map((pokedex) => ({
+    params: { pokedexId: pokedex.name },
+  }));
+
+  return {
+    paths,
+    fallback: true,
+  };
+};
+
+export const getStaticProps = async ({ params }) => {
+  const { pokedexId } = params;
+  const pokedexData = await pokeapiService.getPokedex(pokedexId);
+
+  return {
+    props: {
+      pokedex: pokedexData,
+    },
+  };
+};
+
 export default PokedexPage;
