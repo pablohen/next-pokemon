4b5c00840975354bcb69d2d71ababf3afcf67b90
Pablo Henrique <pablo_hen@hotmail.com>
2021-06-15 15:26:20 -0300

    fix typo

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,8 +6,8 @@ const Home = () => (
   <div>
     <MainMenu />
 
-    <div className="px-4">
-      <p>Please use the main manu. I`ll put some notes here later...</p>
+    <div className="my-2 px-4">
+      <p>Please use the main menu. I`ll put some notes here later...</p>
     </div>
   </div>
 );
